5a47e6e61cc0e31609a36b0f18e51ca4c6233c0c
Mvhemanth2003 <mvhemanth2003@gmail.com>
2025-11-16 19:45:28 +0530

    day 19

--- Day_17/http.js
+++ Day_17/http.js
@@ -1,17 +1,13 @@
-//Creating http server
-const http = require('http');
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to Node.js Server!');
-});
-// Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
-  
-});
+const http  = require('http');
 
 
+// creating a server
+const myServer = http.createServer((request,res) =>
+{
+  res.write("Welcome");
+  res.end();
+  console.log("http://localhost:5000");
 
+})
 
-
+myServer.listen(5000);
