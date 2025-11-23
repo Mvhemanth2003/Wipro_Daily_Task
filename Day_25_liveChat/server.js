9fe860d071a80684f33e541cc0d22553067545d5
Mvhemanth2003 <mvhemanth2003@gmail.com>
2025-11-23 16:27:19 +0530

    day25

---
+++ Day_25_liveChat/server.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const http = require('http');             // Needed to attach socket.io
+const { Server } = require('socket.io');  // Correct socket.io import
+
+const app = express();
+const PORT = 3000;
+
+// Create HTTP server
+const server = http.createServer(app);
+
+// Create Socket.io server with CORS settings
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    }
+});
+
+// Serve static files (HTML, JS, CSS) from "public" folder
+app.use(express.static('public'));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Socket.io connection event
+io.on('connection', (socket) => {
+    console.log("A user connected:", socket.id);
+
+    // Receive message from client
+    socket.on('message', (msg) => {
+        io.emit('message', msg);  // Broadcast to all clients
+    });
+
+    // Disconnect event
+    socket.on('disconnect', () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+// Start server
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
